e966b46e0d93ac7c348bfd2fb7f2701619adeca2
gtfarng <5735512002@psu.ac.th>
2019-02-15 12:25:22 +0700

    deoloy-to-firebase

---
+++ counter-react-app/src/App.js
@@ -0,0 +1,138 @@
+import React, {Component} from 'react'
+import './App.css'
+import './bootstrap.min.css'
+
+class App extends Component
+{
+render()
+{
+  return(
+
+    <div align='center'>
+   
+
+      <Counter />            
+            
+
+    <br></br> 
+    <div class="container name">
+    <div class="row">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8">
+      <h1> Jatupat Pannoi</h1>
+       <h3>#GTfarng</h3>
+    </div>
+    <div class="col-sm-2"></div>
+    </div>
+    </div>
+   
+    </div>
+
+    )
+}
+
+
+}
+export default App
+
+class Counter extends Component
+{   
+  state={Zero:0,Num:0,inum:'NULL'}
+
+   constructor(props)
+  {
+    super(props)
+    this.ops = this.ops.bind(this);
+    this.text = this.text.bind(this);
+    this.Change = this.Change.bind(this);
+    this.Submit = this.Submit.bind(this);
+  }
+  update=(value)=>
+  {
+    value=parseInt(value)
+    this.setState({value})
+  }
+    
+  text=(event)=> 
+  {
+    console.log(event.target.value) 
+  }
+
+  Change=(event)=> 
+  {
+    this.setState({value: event.target.value});
+  }
+
+  Submit=(event)=>
+  {
+    this.setState({Num:this.state.value})
+    event.preventDefault();
+  }
+
+  ops=(c)=>
+  {
+    if(c==="Up")
+    {
+      this.setState({Num:this.state.Num-''+1})
+    }
+    else if(c==="Down")
+    {
+      this.setState({Num:this.state.Num-1})
+    }
+    else if(c==="Reset")
+    {
+      this.setState({Num:this.state.Zero})
+    }
+    
+  }
+
+render()
+{   
+  
+  return(
+
+    <div>
+    
+    <br></br> 
+    <div class="container bg-info">
+    <div class="row">
+    <div class="col-sm-2 "></div>
+    <div class="col-sm-8">
+     
+     <h1 class="display-6 "> Counter App </h1>
+           
+    </div>
+    <div class="col-sm-2 "></div>
+    </div>
+    </div>
+
+    <div class="container bg-light">
+    <div class="row">
+    <div class="col-sm-2 "></div>
+    <div class="col-sm-8">
+     <br></br>
+     <h3>Counter : {this.state.Num} </h3> <br></br>
+
+        <form onSubmit={this.Submit}>
+        <label>
+      
+          <input type="text" placeholder="Enter Number" value={this.state.value} onChange={this.Change} />
+        </label>
+        <input class="btn btn-success" type="submit" value="Set" />
+      </form>
+      
+        <button class="btn btn-primary" onClick={()=> this.ops('Up')}>Up</button>
+        <button class="btn btn-danger" onClick={()=> this.ops('Down')}>Down</button>
+        <button class="btn btn-warning" onClick={()=> this.ops('Reset')}>Reset</button>
+
+    </div>
+    <div class="col-sm-2 "></div>
+    </div>
+    </div>
+
+    </div>
+ 
+          )}
+
+}
+
